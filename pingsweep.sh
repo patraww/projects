429fb1a70a8bd08e928b8d122867bd30230038c9
Will Patraw <49461549+patraww@users.noreply.github.com>
2019-04-09 17:42:26 -0700

    pingsweep.sh
    Simple ping sweep bash script that reports which hosts in a range are online and writes the ICMP traffic to a file using tcpdump.  Homework assignment for a pen testing class.

---
+++ pingsweep.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$1" == "" ]
+
+then
+  echo "Usage:     pingsweep.sh [network][starting-IP][ending-IP]"
+  echo "Example: pingsweep.sh 192.168.29 5 10"
+
+else
+  tcpdump -e icmp[icmptype] == 8 -w ~/home/pingsweep.pcap & #writes icmp traffic to a file
+  sleep 2 #gives tcpdump time to initialize
+
+  for x in `seq $2 $3`; do #loop based on given ip range
+    ping -c 1 $1.$x | grep "64 bytes" | cut -d " " -f4 | sed 's/.$//' #pings given ip address range and outputs online hosts
+  done
+
+  sleep 2 #gives tcpdump time to write
+  pid=$(ps -e | pgrep tcpdump) #locates the process id of tcpdump
+  kill -2 $pid #shuts down tcpdump
+fi
